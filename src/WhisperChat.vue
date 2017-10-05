09a23e88a6720074259203b50a6018c023c260fa
Guillaume Ballet <gballet@gmail.com>
2017-10-05 18:10:00 +0200

    Support for asymmetric messaging

--- src/WhisperChat.vue
+++ src/WhisperChat.vue
@@ -2,13 +2,20 @@
 	<div>
 		<h1>Whisper Example Chat Application</h1>
 		<div v-if="!configured">
-			Key generation password: <input v-model="sympw" @input="updateKey" /><br>
+			<input type="checkbox" v-model="asym" /> Asymmetric<br>
+			<asymmetric-key-config v-if="asym" pub-key="asymPubKey" key-id="asymKeyId"></asymmetric-key-config>
+			<symmetric-key-config v-else @update-sym-key="updateSymKey" :sym-key-id="symKeyId"></symmetric-key-config>
+
 			username: <input v-model="name" /><br>
-			Key: {{keyEdit}}<br>
-			<button @click="configWithKey">Start</button>
+			<button @click="configWithKey" v-if="(asymKeyId || symKeyId) && name">Start</button>
 		</div>
 		<div v-else>
-			Key: {{key}}<br>
+			<div v-if="asym">
+				Recipient's public key: <input  v-model="recipientPubKey" />
+			</div>
+			<div v-else>
+				Key: {{symKeyId}}
+			</div>
 			<p v-for="m of msgs">
 				<b>{{m.name}}</b>: {{m.text}}
 			</p>
@@ -20,6 +27,8 @@
 
 <script>
 import Web3 from 'web3';
+import SymmetricKeyConfig from './SymmetricKeyConfig.vue';
+import AsymmetricKeyConfig from './AsymmetricKeyConfig.vue';
 import {decodeFromHex, encodeToHex} from './hexutils';
 
 const defaultRecipientPubKey = "0x04ffb2647c10767095de83d45c7c0f780e483fb2221a1431cb97a5c61becd3c22938abfe83dd6706fc1154485b80bc8fcd94aea61bf19dd3206f37d55191b9a9c4";
@@ -33,17 +42,26 @@ export default {
 		let data = {
 			msgs: [],
 			text: "",
-			keyEdit: null,
-			key: null,
+			symKeyId: null,
 			name: "",
+			asymKeyId: null,
 			sympw: "",
+			asym: true,
 			configured: false,
 			topic: defaultTopic,
-			recipientPubKey: defaultRecipientPubKey
+			recipientPubKey: defaultRecipientPubKey,
+			asymPubKey: ""
 		};
+
+		this.shh.newKeyPair().then(id => {
+			data.asymKeyId = id;
+			return this.shh.getPublicKey(id).then(pubKey => this.asymPubKey = pubKey).catch(console.log);
+		}).catch(console.log);
 
 		return data;
 	},
+
+	components: {AsymmetricKeyConfig, SymmetricKeyConfig},
 
 	methods: {
 		sendMessage() {
@@ -60,15 +78,21 @@ export default {
 				powTarget: 2.01,
 				powTime: 100,
 				payload: encodeToHex(JSON.stringify(msg)),
-				postData.symKeyID = this.keyEdit;
+			};
+
+			if (this.asym) {
+				postData.pubKey = this.recipientPubKey;
+				postData.sig = this.asymKeyId;
+			} else
+				postData.symKeyID = this.symKeyId;
 
 			this.shh.post(postData);
 
 			this.text = "";
 		},
 
-		updateKey() {
-			this.shh.generateSymKeyFromPassword(this.sympw).then(symKeyID => this.keyEdit = symKeyID)
+		updateSymKey(sympw) {
+			this.shh.generateSymKeyFromPassword(sympw).then(symKeyID => this.symKeyId = symKeyID)
 		},
 
 		configWithKey() {
@@ -81,16 +105,22 @@ export default {
 			let filter = {
 				topics: ['0xdeadbeef']
 			};
+
+			if (this.asym) {
+				if(!this.asymKeyId) {
+					alert("No valid asymmetric key");
 				return;
 			}
 
-				if (!this.keyEdit || this.keyEdit.length == 0) {
+				filter.privateKeyID = this.asymKeyId;
+			} else {
+				if (!this.symKeyId || this.symKeyId.length == 0) {
 					alert("please enter a pasword to generate a key!");
 				return;
 			}
 
-			this.key = this.keyEdit;
-				filter.symKeyID = this.keyEdit;
+				filter.symKeyID = this.symKeyId;
+			}
 
 			this.msgFilter = this.shh.newMessageFilter(filter).then(filterId => {
 				setInterval(() => {
